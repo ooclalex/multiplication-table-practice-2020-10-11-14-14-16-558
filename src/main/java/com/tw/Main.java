e1ea614dabe4af1a02d9c50179cdb64dc5c64d1d
Alex Liu <LIUAL14@OOCL.COM>
2020-11-24 17:35:41 +0800

    multiplication table

---
+++ src/main/java/com/tw/Main.java
@@ -0,0 +1,8 @@
+package com.tw;
+
+public class Main {
+    public static void main(String[] args) {
+        MultiplicationTable m = new MultiplicationTable();
+        System.out.println(m.create(2, 5));
+    }
+}
--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,49 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
-        return null;
+        boolean isStartSmallerThanEnd = isStartSmallerThanEnd(startNumber, endNumber);
+        boolean areStartAndEndInRange = areStartAndEndInRange(startNumber, endNumber);
+        if (!isStartSmallerThanEnd || !areStartAndEndInRange) return null;
+
+        return generateTable(startNumber, endNumber);
+    }
+
+    private boolean areStartAndEndInRange(int startNumber, int endNumber) {
+        return 1 <= startNumber && startNumber <= 1000 &&
+                1 <= endNumber && endNumber <= 1000;
+    }
+
+    private boolean isStartSmallerThanEnd(int startNumber, int endNumber) {
+        return startNumber <= endNumber;
+    }
+
+    private String generateExpression(int multiplier, int multiplicand) {
+        String product = multiplier + "*" + multiplicand + "=" + multiplier*multiplicand;
+        if (multiplier != multiplicand) {
+            product += "  ";
+        }
+        return product;
+    }
+
+    private String generateLine(int multiplier, int row) {
+        StringBuilder strB = new StringBuilder();
+        while (multiplier <= row) {
+            strB.append(generateExpression(multiplier, row));
+            multiplier++;
+        }
+        return strB.toString();
+    }
+
+    private String generateTable(int start, int end) {
+        StringBuilder strB = new StringBuilder();
+        int row = start;
+        while (start <= end) {
+            strB.append(generateLine(row, start));
+            if (start != end) {
+                strB.append("\r\n");
+            }
+            start++;
+        }
+        return strB.toString();
     }
 }
